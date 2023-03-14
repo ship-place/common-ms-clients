81fd2f9d9eab73c1d20d3cec635ca95d67a08cda
Lexx Xell <lexxxell007@gmail.com>
2023-03-14 19:30:01 +0400

    v0.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { MsChangesClientModule } from './ms-changes-client/ms-changes-client.module';
 export { MsChangesClientService } from './ms-changes-client/ms-changes-client.service';
 export { ChangesAction } from './ms-changes-client/enums/changes-action.enum';
+export { MsAuthClientModule } from './ms-auth-client/ms-auth-client.module';
+export { MsAuthClientService } from './ms-auth-client/ms-auth-client.service';
